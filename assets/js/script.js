46fa20b16eca3406297accaae0fef2bdbd212b51
darsay8 <darsay.one@gmail.com>
2020-08-08 09:57:54 -0400

    new

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+    $("a").click(function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            let hash = this.hash;
+
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
+});
